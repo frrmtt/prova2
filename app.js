c8a42acc8adafe25b3f0f8c397fb6ecb4dca0855
Matteo <matteo.frare@studenti.unitn.it>
2019-01-12 19:45:19 +0100

    programma funzionante su un solo file

--- app.js
+++ app.js
@@ -2,6 +2,29 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const bodyParser = require('body-parser');
+
+app.use( bodyParser.json() );
+
+
 app.listen(PORT, () => console.log('Example app listening on port '+ PORT));
 
 app.get('/', (req, res) => res.send('Hello World!'));
+let result;
+
+function string_square(s){
+    
+ if (isNaN(s)){
+    return s.length*s.length;
+
+ }else return -1;
+};
+
+app.get('/square', (req, res) => {
+
+    let parola = req.query.string;
+    result = string_square(parola);
+
+
+    res.status(200).json(result);
+    });
---
+++ square.js
@@ -0,0 +1,9 @@
+/*unction string_square(s){
+    
+ if (isNaN(s)){
+    return s.length*s.length;
+
+ }else return -1;
+};
+
+module.export = string_square;*/
